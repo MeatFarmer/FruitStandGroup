6d0e99c38b2f0448bee84c5afa98d5f9a6882158
Joseph Barsness <joseph.barsness@gmail.com>
2016-12-01 16:47:22 -0600

    updated timer to 15 seconds

--- scripts/script.js
+++ scripts/script.js
@@ -6,20 +6,20 @@ setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
   $('#my_box1').html( "$ " + number.toFixed(2) );
 },
-1000);
+15000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
   $('#my_box2').html( "$ " + number.toFixed(2) );
 },
-1000);
+15000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
   $('#my_box3').html( "$ " + number.toFixed(2) );
 },
-1000);
+15000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
   $('#my_box4').html( "$ " + number.toFixed(2) );
 },
-1000);
+15000);
 setInterval(0);
