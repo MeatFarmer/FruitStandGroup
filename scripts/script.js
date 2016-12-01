fac64dd055c9d2fd7323702693abdf1567e10142
Joseph Barsness <joseph.barsness@gmail.com>
2016-12-01 16:41:39 -0600

    formatted random number generator

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
 }); // end document ready function
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
-  $('#my_box1').html( "$ " + number );
+  $('#my_box1').html( "$ " + number.toFixed(2) );
 },
 1000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
-  $('#my_box2').html( "$ " + number );
+  $('#my_box2').html( "$ " + number.toFixed(2) );
 },
 1000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
-  $('#my_box3').html( "$ " + number );
+  $('#my_box3').html( "$ " + number.toFixed(2) );
 },
 1000);
 setInterval(function() {
   var number = 0.50 + Math.floor(Math.random() * 9.99);
-  $('#my_box4').html( "$ " + number );
+  $('#my_box4').html( "$ " + number.toFixed(2) );
 },
 1000);
 setInterval(0);
